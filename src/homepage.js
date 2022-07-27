754f2bcf1883e1aac789ac3b60fc46df25ea380b
ijzzBloc <mhdejong91@gmail.com>
2022-07-27 23:38:59 +0200

    added search function

---
+++ src/homepage.js
@@ -0,0 +1,10 @@
+
+import  {
+    fetchData
+} from "./main";
+
+let userInput = document.getElementById('search');
+let searchSubmit = document.getElementById('search-bttn')
+searchSubmit.addEventListener('click', () => {
+    fetchData(userInput.value)
+})
--- src/main.js
+++ src/main.js
@@ -1,2 +1,33 @@
 require('dotenv').config()
-import axios from 'axios';
+const axios = require('axios').default;
+
+//********************-********************//
+export async function fetchData(query) {
+    try {
+        // const res = await axios.get('https://api.edamam.com/api/recipes/v2?type=public&q=%22chicken%22&app_id=9401dfa0&app_key=' + process.env.API_KEY + '&random=true')
+        const res2 = await axios.get('https://api.edamam.com/api/recipes/v2',
+            {
+                params:
+                    {
+                        type: 'public',
+                        q: query,
+                        app_id: process.env.API_ID,
+                        app_key: process.env.API_KEY,
+                    }
+            }).then((result) => {
+            console.log(result)
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+
+
+
+
+
+
+
+
+
