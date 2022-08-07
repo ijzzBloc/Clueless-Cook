bf583736a8c767c9d484be7cc533f04f41f35991
ijzzBloc <mhdejong91@gmail.com>
2022-08-07 03:44:50 +0200

    small update

--- dist/kcal-calc.8fb09ab1.js
+++ dist/kcal-calc.8fb09ab1.js
@@ -535,11 +535,17 @@ function hmrAcceptRun(bundle, id) {
 var _main = require("./main");
 let calcInput = document.getElementById("kcalsearch");
 let calcSubmit = document.getElementById("kcal-search-bttn");
+let infoContainer = document.getElementById("info-container");
 calcSubmit.addEventListener("click", ()=>{
     let calcInputText = calcInput.value;
     if (calcInputText !== "") (0, _main.calcData)(calcInputText).then((result)=>{
+        infoContainer.innerHTML = "";
         let kcalResult = result;
         console.log(kcalResult.data.parsed);
+        let kcalHTML = "";
+        let kcalObject = kcalResult.data.parsed;
+        kcalHTML += '<div id="info">\n<table class="resultstable">\n<tr>\n<th>Product</th>\n<th>Quantity</th>\n<th>Label</th>\n</tr>\n<tr>\n<td>Mars</td>\n<td>60</td>\n<td>Gram</td>\n</tr>\n</table>\n</div>';
+        infoContainer.innerHTML = kcalHTML;
     }).catch((reason)=>{
         alert(reason);
     });
--- src/kcal-calc.js
+++ src/kcal-calc.js
@@ -2,12 +2,32 @@ import {calcData} from "./main";
 
 let calcInput = document.getElementById('kcalsearch');
 let calcSubmit = document.getElementById('kcal-search-bttn')
+let infoContainer = document.getElementById('info-container')
 calcSubmit.addEventListener('click', () => {
     let calcInputText = calcInput.value;
     if (calcInputText !== "") {
         calcData(calcInputText).then((result) => {
+            infoContainer.innerHTML = "";
             let kcalResult = result;
             console.log(kcalResult.data.parsed)
+            let kcalHTML = ""
+            let kcalObject = kcalResult.data.parsed;
+            kcalHTML +=
+                '<div id="info">\n' +
+                    '<table class="resultstable">\n' +
+                        '<tr>\n' +
+                            '<th>Product</th>\n' +
+                            '<th>Quantity</th>\n' +
+                            '<th>Label</th>\n' +
+                        '</tr>\n' +
+                        '<tr>\n' +
+                            '<td>Mars</td>\n' +
+                            '<td>60</td>\n' +
+                            '<td>Gram</td>\n' +
+                        '</tr>\n' +
+                    '</table>\n' +
+                '</div>'
+            infoContainer.innerHTML = kcalHTML;
         })
             .catch(reason => {
                 alert(reason)
